9951cb0cdfbfc8ad04cebdcb55edd31990f0c400
Dave-Palmer <djarmor@gmail.com>
2024-07-05 23:26:36 +0100

    First commit, app work using local storage

---
+++ src/Note.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useNote } from "./NoteLayout";
+import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import ReactMarkDown from "react-markdown";
+
+type NoteProps = {
+  onDelete: (id: string) => void;
+};
+
+const Note = ({ onDelete }: NoteProps) => {
+  const note = useNote();
+  const navigate = useNavigate();
+  return (
+    <>
+      <Row className="align-items-center mb-4">
+        <Col>
+          <h1>{note.title}</h1>
+
+          {note.tags.length > 0 && (
+            <Stack gap={1} direction="horizontal" className="flex-wrap">
+              {note.tags.map((tag) => (
+                <Badge className="text-truncates" key={tag.id}>
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Col>
+        <Col xs="auto">
+          <Stack direction="horizontal" gap={2}>
+            <Link to={`/${note.id}/edit`}>
+              <Button variant="primary">Edit</Button>
+            </Link>
+            <Button
+              variant="outline-danger"
+              onClick={() => {
+                onDelete(note.id);
+                navigate("/");
+              }}>
+              Delete
+            </Button>
+            <Link to={".."}>
+              <Button variant="outline-secondary">back</Button>
+            </Link>
+          </Stack>
+        </Col>
+      </Row>
+      <ReactMarkDown>{note.markdown}</ReactMarkDown>
+    </>
+  );
+};
+
+export default Note;
